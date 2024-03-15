58af025f82a9ff05df29aa8f221fd160368e6e77
Natnael@123 <natnaelmverse1@gmail.com>
2024-03-15 16:54:02 +0300

    naming modified

--- createTopic.js
+++ createTopic.js
@@ -1,7 +1,7 @@
 const namespaces = ["Ethiopia/BPO","Ethiopia/individual_company","Ethiopia/international_company","Ethiopia/local_agency"];
 
-// a reusable topic creator function
-const createTopics = async (namespace, client, topic) => {
+// a reusable producer creator function
+const createProducer = async (namespace, client, topic) => {
   return client.createProducer({
         topic: `persistent://${namespace}/${topic}`,
         producerName: namespace.split('/')[1].toString(),
